71b3fb7bf7dc6153c4350c3866787e397018cef5
Nicolle Nogueira da Silva <nicollenogueirasilva06@gmail.com>
2025-09-18 22:03:32 -0300

    20250918

---
+++ 20250918/Atividade.DML.sql
@@ -0,0 +1,24 @@
+1) Inserindo dados de clientes
+
+INSERT INTO CLIENTES (Nome, Email) VALUES ('Nicolle Nogueira', 'nicolle.nsilva@gmail.br');
+INSERT INTO CLIENTES (Nome, Email) VALUES ('Sofia Almeida', 'sofi.almeda@hotmail.com');
+INSERT INTO CLIENTES (Nome) VALUES ('Jose Campos');
+
+2) Atualizando preços de produtos
+
+UPDATE PRODUTOS SET preco = preco*1.10  WHERE tipo = 'Notebook';
+
+3) Removendo pedidos cancelados
+
+DELETE FROM PEDIDOS WHERE status = 'CANCELADO';
+
+4) Usando MERGE para atualizar clientes
+
+MERGE INTO CLIENTES A 
+USING CLIENTES_STG B
+ON (A.Email = B.Email)
+WHEN MATCHED THEN
+    UPDATE SET A.Nome = B.Nome 
+WHEN NOT MATCHED THEN 
+    INSERT (Nome, Email)
+    VALUES (B.Nome, B.Email);
